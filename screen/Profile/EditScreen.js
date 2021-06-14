dfc59d879bada466a0af692d8a582866b5d35ef3
augustustung <huytung139@gmail.com>
2021-06-14 13:45:46 +0700

    updating edit profile

--- screen/Profile/EditScreen.js
+++ screen/Profile/EditScreen.js
@@ -1,14 +1,61 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { TouchableOpacity
+, Text, View, KeyboardAvoidingView, Platform, Keyboard } from 'react-native'
+import FormInput from '../../Components/FormInput'
+import FormButton from '../../Components/FormButton'
+import { UserImg } from '../styled/styledProfile'
+import { SCREEN_WIDTH } from '../../ultis/Dimentions'
 
 const EditScreen = () => {
+    const [firstName, setFirstName] = useState('Tung')
+    const [lastName, setLastName] = useState('Bui Huy')
+    const [email, setEmail] = useState('huybi')
+    const [phone, setPhone] = useState('')
+    const [bio, setBio] = useState('')
+
     return (
-        <View>
-            <Text></Text>
-        </View>
+            <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"} style={{width: SCREEN_WIDTH * 0.9, alignSelf: 'center', flex: 1}}>
+                <TouchableOpacity activeOpacity={1} onPress={Keyboard.dismiss}>
+                    <UserImg source={require("../../images/default-avartar.png")}/>
+                    <FormInput 
+                        iconName={"user"}
+                        labelVal={firstName} 
+                        onChangeText={(text) => setFirstName(text)}
+                    />
+                    <FormInput 
+                        iconName={"user"}
+                        labelVal={lastName} 
+                        onChangeText={(text) => setLastName(text)}
+                    />
+                    <FormInput 
+                        iconName={"user"}
+                        labelVal={email} 
+                        onChangeText={(text) => setEmail(text)}
+                        keyboardType={"email-address"}
+                    />
+                    <FormInput
+                        iconName={"phone"}
+                        labelVal={phone} 
+                        onChangeText={(text) => setPhone(text)}
+                        keyboardType={"numeric"}
+                    />
+                    <FormInput 
+                        iconName={"user"}
+                        labelVal={bio} 
+                        onChangeText={(text) => setBio(text)}
+                        keyboardType={"email-address"}
+                    />
+
+                    <FormButton 
+                        buttonTitle="Update"
+                        onPress={() => {}}
+                    />
+                </TouchableOpacity>
+            </KeyboardAvoidingView>
+            
+        
     )
 }
 
 export default EditScreen
-
-const styles = StyleSheet.create({})
